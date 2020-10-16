a2e1ae9f0d4016707fa72e71eeaa131d70f2ade3
Lisa Backer <lisa.haas.backer@gmail.com>
2020-10-16 10:18:04 -0400

    useeffect

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
+import React, { useEffect, useState } from 'react';
+import pet, { ANIMALS } from '@frontendmasters/pet';
 import useDropdown from './useDropdown';
 
 const SearchParams = () => {
@@ -7,7 +7,17 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
 
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed('');
+
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedNames = breeds.map(({ name }) => name);
+      setBreeds(breedNames);
+    }, console.error);
+  }, [animal, setBreed, setBreeds])
 
   return (
     <div className="search-params">
