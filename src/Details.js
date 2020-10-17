5d2af412aecdee666cb341695b91902b2a1e5621
Lisa Backer <lisa.haas.backer@gmail.com>
2020-10-17 15:02:16 -0400

    theme context

--- src/Details.js
+++ src/Details.js
@@ -2,12 +2,12 @@ import React from 'react';
 import pet from '@frontendmasters/pet';
 import Carousel from './Carousel';
 import ErrorBoundary from './ErrorBoundary';
+import ThemeContext from './ThemeContext';
 
 class Details extends React.Component {
   state = { loading: true };
 
   componentDidMount() {
-    throw new Error("Crazy Harry");
     pet.animal(this.props.id)
       .then(({ animal }) => {
         this.setState({
@@ -34,7 +34,12 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+
+          <ThemeContext.Consumer>
+            {([themeHook]) => (
+              <button style={{backgroundColor: themeHook }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
